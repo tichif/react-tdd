31f0f909294fffe72e409661b9105b767d7513a1
Charles-Hébert Dalzon <charlby5@gmail.com>
2022-05-11 15:03:08 -0400

    Add style

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -34,41 +34,71 @@ class SignUpPage extends Component {
     }
 
     return (
-      <>
-        <form>
-          <h1>Sign Up</h1>
-          <label htmlFor='username'>Username</label>
-          <input
-            placeholder='Username'
-            id='username'
-            onChange={this.onChangeHandler}
-          />
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            placeholder='Email'
-            id='email'
-            onChange={this.onChangeHandler}
-          />
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            placeholder='Password'
-            id='password'
-            onChange={this.onChangeHandler}
-          />
-          <label htmlFor='confirmPassword'>Confirm Password</label>
-          <input
-            type='password'
-            placeholder='Confirm Password'
-            id='confirmPassword'
-            onChange={this.onChangeHandler}
-          />
-          <button disabled={disabled} onClick={this.clickHandler}>
-            Sign Up
-          </button>
+      <div className='col-lg-6 offset-lg-3 col-md-8 offset-md-2'>
+        <form className='card mt-5'>
+          <div className='card-header'>
+            <h1 className='text-center'>Sign Up</h1>
+          </div>
+          <div className='card-body'>
+            <div className='mb-3'>
+              <label htmlFor='username' className='form-label'>
+                Username
+              </label>
+              <input
+                placeholder='Username'
+                id='username'
+                onChange={this.onChangeHandler}
+                className='form-control'
+              />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='email' className='form-label'>
+                Email
+              </label>
+              <input
+                type='email'
+                placeholder='Email'
+                id='email'
+                onChange={this.onChangeHandler}
+                className='form-control'
+              />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='password' className='form-label'>
+                Password
+              </label>
+              <input
+                type='password'
+                placeholder='Password'
+                id='password'
+                onChange={this.onChangeHandler}
+                className='form-control'
+              />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='confirmPassword' className='form-label'>
+                Confirm Password
+              </label>
+              <input
+                type='password'
+                placeholder='Confirm Password'
+                id='confirmPassword'
+                onChange={this.onChangeHandler}
+                className='form-control'
+              />
+            </div>
+            <div className='text-center'>
+              <button
+                disabled={disabled}
+                onClick={this.clickHandler}
+                className='btn btn-primary'
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
         </form>
-      </>
+      </div>
     );
   }
 }
