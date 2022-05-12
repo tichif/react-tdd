ad18554e9b570d3262a557d9b3c987739e7c7e98
Charles-Hébert Dalzon <charlby5@gmail.com>
2022-05-11 22:00:13 -0400

    Hide the form when the submission is successful

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -8,6 +8,7 @@ class SignUpPage extends Component {
     password: '',
     confirmPassword: '',
     loading: false,
+    success: false,
   };
 
   onChangeHandler = (e) => {
@@ -17,99 +18,107 @@ class SignUpPage extends Component {
     });
   };
 
-  clickHandler = (e) => {
+  clickHandler = async (e) => {
     e.preventDefault();
     const { username, email, password } = this.state;
     this.setState({ loading: true });
-    axios.post('/api/1.0/users', {
+    await axios.post('/api/1.0/users', {
       username,
       email,
       password,
     });
+    this.setState({ success: true });
   };
 
   render() {
     let disabled = true;
-    const { password, confirmPassword, loading } = this.state;
+    const { password, confirmPassword, loading, success } = this.state;
     if (password && confirmPassword) {
       disabled = password !== confirmPassword;
     }
 
     return (
       <div className='col-lg-6 offset-lg-3 col-md-8 offset-md-2'>
-        <form className='card mt-5'>
-          <div className='card-header'>
-            <h1 className='text-center'>Sign Up</h1>
+        {!success && (
+          <form className='card mt-5' data-testid='form-sign-up'>
+            <div className='card-header'>
+              <h1 className='text-center'>Sign Up</h1>
+            </div>
+            <div className='card-body'>
+              <div className='mb-3'>
+                <label htmlFor='username' className='form-label'>
+                  Username
+                </label>
+                <input
+                  placeholder='Username'
+                  id='username'
+                  onChange={this.onChangeHandler}
+                  className='form-control'
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='email' className='form-label'>
+                  Email
+                </label>
+                <input
+                  type='email'
+                  placeholder='Email'
+                  id='email'
+                  onChange={this.onChangeHandler}
+                  className='form-control'
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='password' className='form-label'>
+                  Password
+                </label>
+                <input
+                  type='password'
+                  placeholder='Password'
+                  id='password'
+                  onChange={this.onChangeHandler}
+                  className='form-control'
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='confirmPassword' className='form-label'>
+                  Confirm Password
+                </label>
+                <input
+                  type='password'
+                  placeholder='Confirm Password'
+                  id='confirmPassword'
+                  onChange={this.onChangeHandler}
+                  className='form-control'
+                />
+              </div>
+              <div className='text-center'>
+                <button
+                  disabled={disabled || loading}
+                  onClick={this.clickHandler}
+                  className='btn btn-primary'
+                >
+                  {loading ? (
+                    <>
+                      <span
+                        className='spinner-border spinner-border-sm'
+                        role='status'
+                      ></span>
+                      Loading...
+                    </>
+                  ) : (
+                    'Sign Up'
+                  )}
+                </button>
+              </div>
+            </div>
+          </form>
+        )}
+        {success && (
+          <div className='alert alert-success mt-3'>
+            Please check your e-mail to activate your account
           </div>
-          <div className='card-body'>
-            <div className='mb-3'>
-              <label htmlFor='username' className='form-label'>
-                Username
-              </label>
-              <input
-                placeholder='Username'
-                id='username'
-                onChange={this.onChangeHandler}
-                className='form-control'
-              />
-            </div>
-            <div className='mb-3'>
-              <label htmlFor='email' className='form-label'>
-                Email
-              </label>
-              <input
-                type='email'
-                placeholder='Email'
-                id='email'
-                onChange={this.onChangeHandler}
-                className='form-control'
-              />
-            </div>
-            <div className='mb-3'>
-              <label htmlFor='password' className='form-label'>
-                Password
-              </label>
-              <input
-                type='password'
-                placeholder='Password'
-                id='password'
-                onChange={this.onChangeHandler}
-                className='form-control'
-              />
-            </div>
-            <div className='mb-3'>
-              <label htmlFor='confirmPassword' className='form-label'>
-                Confirm Password
-              </label>
-              <input
-                type='password'
-                placeholder='Confirm Password'
-                id='confirmPassword'
-                onChange={this.onChangeHandler}
-                className='form-control'
-              />
-            </div>
-            <div className='text-center'>
-              <button
-                disabled={disabled || loading}
-                onClick={this.clickHandler}
-                className='btn btn-primary'
-              >
-                {loading ? (
-                  <>
-                    <span
-                      className='spinner-border spinner-border-sm'
-                      role='status'
-                    ></span>
-                    Loading...
-                  </>
-                ) : (
-                  'Sign Up'
-                )}
-              </button>
-            </div>
-          </div>
-        </form>
+        )}
       </div>
     );
   }
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,4 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
@@ -114,7 +119,9 @@ describe('Sign Up page', () => {
 
       userEvent.click(button);
 
-      await new Promise((resolve) => setTimeout(resolve, 500));
+      await screen.findByText(
+        'Please check your e-mail to activate your account'
+      );
 
       expect(requestBody).toEqual({
         username: 'Tichif',
@@ -141,7 +148,9 @@ describe('Sign Up page', () => {
       userEvent.click(button);
       userEvent.click(button);
 
-      await new Promise((resolve) => setTimeout(resolve, 500));
+      await screen.findByText(
+        'Please check your e-mail to activate your account'
+      );
 
       expect(counter).toBe(1);
     });
@@ -168,6 +177,54 @@ describe('Sign Up page', () => {
       spinner = screen.getByRole('status');
 
       expect(spinner).toBeInTheDocument();
+      await screen.findByText(
+        'Please check your e-mail to activate your account'
+      );
+    });
+
+    it('displays account creation notification after successful sign up', async () => {
+      // create a fake server
+
+      const server = setupServer(
+        rest.post('/api/1.0/users', (req, res, ctx) => {
+          return res(ctx.status(200));
+        })
+      );
+      server.listen();
+
+      setup();
+
+      const message = 'Please check your e-mail to activate your account';
+
+      expect(screen.queryByText(message)).not.toBeInTheDocument();
+
+      userEvent.click(button);
+
+      const text = await screen.findByText(message);
+      expect(text).toBeInTheDocument();
+    });
+
+    it('hides sign up form after successful sign up request', async () => {
+      // create a fake server
+      const server = setupServer(
+        rest.post('/api/1.0/users', (req, res, ctx) => {
+          return res(ctx.status(200));
+        })
+      );
+      server.listen();
+
+      setup();
+
+      const form = screen.getByTestId('form-sign-up');
+      userEvent.click(button);
+
+      // first way
+      // await waitFor(() => {
+      //   expect(form).not.toBeInTheDocument();
+      // });
+
+      // second way
+      await waitForElementToBeRemoved(form);
     });
   });
 });
